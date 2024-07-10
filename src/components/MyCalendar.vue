08f16924f8fd60c41e37cbdf25c79aa3aae07ea5
endan <lsalamante@yondu.com>
2024-07-10 12:56:03 +0800

    init storybook

---
+++ src/components/MyCalendar.vue
@@ -0,0 +1,17 @@
+<template>
+  <Calendar borderless transparent expanded class="my-calendar" />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Calendar } from 'v-calendar'
+import 'v-calendar/style.css'
+
+const date = ref(new Date())
+</script>
+
+<style scoped>
+.my-calendar {
+  display: none;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { setupCalendar } from 'v-calendar'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+// Use calendar defaults (optional)
+app.use(setupCalendar, {})
 
 app.mount('#app')
