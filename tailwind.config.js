c311e7ed52ae78582429647c584f01aaa0458fde
endan <lsalamante@yondu.com>
2024-07-10 14:54:16 +0800

    fix default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
           300: '#008DF0',
           400: '#0062A8',
           500: '#005694',
-          default: '#008DF0'
+          DEFAULT: '#008DF0'
         },
         'hello-yellow': {
           50: '#FFF8EB',
@@ -22,7 +22,7 @@ export default {
           300: '#FFB838',
           400: '#B35927',
           500: '#81401C',
-          default: '#151C36'
+          DEFAULT: '#151C36'
         },
         pink: {
           50: '#FBECF1',
@@ -32,7 +32,7 @@ export default {
           300: '#DA4175',
           400: '#992E52',
           500: '#852847',
-          default: '#DA4175'
+          DEFAULT: '#DA4175'
         },
         purple: {
           50: '#F0ECF4',
@@ -42,7 +42,7 @@ export default {
           300: '#6D3D91',
           400: '#4C2B66',
           500: '#422558',
-          default: '#6D3D91'
+          DEFAULT: '#6D3D91'
         },
         teal: {
           50: '#422558',
@@ -52,7 +52,7 @@ export default {
           300: '#18E2CE',
           400: '#119E90',
           500: '#0F8A7E',
-          default: '#18E2CE'
+          DEFAULT: '#18E2CE'
         }
       }
     }
