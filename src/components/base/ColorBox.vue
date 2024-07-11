7cf3a44c0a4996e0006640e4ca1ae46a0e14d86d
endan <lsalamante@yondu.com>
2024-07-11 08:07:47 +0800

    add colorbox and try to make fonts work

---
+++ src/components/base/ColorBox.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex h-[182px] w-[152px] flex-col gap-y-3 border border-gray-100 p-4">
+    <div class="flex h-[120px] w-[120px] flex-col justify-end bg-teal-100 px-1.5 py-2">
+      <p>#12345</p>
+    </div>
+    <p>Test</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
