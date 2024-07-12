da59e6a91ddc1fa690722d82348d5d6ac71024ed
Ro11ingButler <rollingkai1214@gmail.com>
2024-07-12 10:19:37 +0800

    feat: add css vs tw demo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import ColorBox from '@/components/base/ColorBox.vue'
-</script>
-
 <template>
-  <div class="justify center flex h-screen w-screen items-center">
-    <h1 class="text-techy-blue-300 w-full text-center text-3xl font-bold underline">
+  <div class="justify-center flex flex-col h-screen w-screen items-center gap-4">
+    <h1 class="text-techy-blue-300 w-full text-center text-3xl font-bold underline hidden">
       Hello World!
     </h1>
-    <ColorBox selected-color="bg-brand-techy-blue-400" />
+    <MyComponent></MyComponent>
+    <MyComponentCss></MyComponentCss>
   </div>
 
   <!-- <RouterView /> -->
 </template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import ColorBox from '@/components/base/ColorBox.vue'
+import MyComponent from './components/example/MyComponent.vue';
+import MyComponentCss from './components/example/MyComponentCss.vue'
+</script>
---
+++ src/components/example/MyComponentCss.vue
@@ -0,0 +1,100 @@
+<template>
+    <article>
+        <header>
+            <img src="@/assets/announcement.svg" />
+            <div>
+                <h2>Campaigns</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod,
+                    consequatur, autem
+                    maiores odio voluptatum iure perspiciatis temporibus cupiditate aliquam magni deleniti? Beatae
+                    deleniti, iusto quo quam ea quae incidunt amet.</p>
+            </div>
+            <img src="@/assets/right-arrow.svg" />
+        </header>
+        <section>
+            <div>
+                <p>100m</p>
+                <p>Running</p>
+            </div>
+            <div>
+                <p>9m</p>
+                <p>Scheduled</p>
+            </div>
+        </section>
+    </article>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+article {
+    padding: 1.5em;
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+    border: 1px solid #e8e8e8;
+    border-radius: 12px 12px 24px 12px;
+    max-width: 396px;
+}
+
+header {
+    display: flex;
+    align-items: center;
+    gap: 0.5em;
+
+    h2 {
+        font-size: 1.5em;
+        font-weight: 900;
+        line-height: 30px;
+        letter-spacing: -2%;
+    }
+
+    p {
+        font-weight: 500;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+
+    div {
+        display: flex;
+        flex-direction: column;
+        gap: 2px;
+        max-width: 260px;
+    }
+}
+
+section {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+    --tw-border-opacity: 1;
+    border-color: rgb(232 232 232 / var(--tw-border-opacity));
+
+    div {
+        display: flex;
+        flex-direction: column;
+
+        &:nth-child(2) {
+            padding-left: 1.5em;
+            --tw-divide-x-reverse: 0;
+            border-right-width: calc(1px * var(--tw-divide-x-reverse));
+            border-left-width: calc(1px * calc(1 - var(--tw-divide-x-reverse)));
+        }
+
+        p {
+            font-size: 14px;
+
+            &:nth-child(1) {
+                font-size: 39px;
+                font-weight: 600;
+            }
+
+            &:nth-child(2) {
+                font-weight: 500;
+            }
+        }
+
+
+    }
+}
+</style>
