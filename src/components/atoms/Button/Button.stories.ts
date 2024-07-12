774cb2c5a51d4158ddacf3e7adb22314a6c05474
Rhannel Dinlasan <rdinlasan@yondu.com>
2024-07-12 14:43:07 +0800

    [feature] button storybook update

--- src/components/atoms/Button/Button.stories.ts
+++ src/components/atoms/Button/Button.stories.ts
@@ -1,6 +1,7 @@
-import type { Meta, StoryObj } from '@storybook/vue3';
-import { fn } from '@storybook/test';
-import Button from './Button.vue';
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { fn } from '@storybook/test'
+import Button from './Button.vue'
+import ChevronDown from '@/components/icons/fontawesome/ChevronDown.vue'
 
 const meta = {
   title: 'Atoms/Button',
@@ -9,16 +10,71 @@ const meta = {
   args: {
     primary: false,
     disabled: false,
-    onClick: fn(),
-  },
-} satisfies Meta<typeof Button>;
+    onClick: fn()
+  }
+} satisfies Meta<typeof Button>
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+export default meta
+type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
   args: {
     primary: true,
-    label: 'Button',
+    label: 'Button'
+  }
+}
+
+export const LeftIcon: Story = {
+  args: {
+    primary: true,
+    label: 'Button'
   },
-};
+  render: (args) => ({
+    components: { Button, ChevronDown },
+    setup() {
+      return { args }
+    },
+    template: `
+      <Button v-bind="args">
+        <template v-slot:icon-left><ChevronDown/></template>
+      </Button>
+    `
+  })
+}
+
+export const RightIcon: Story = {
+  args: {
+    primary: true,
+    label: 'Button'
+  },
+  render: (args) => ({
+    components: { Button, ChevronDown },
+    setup() {
+      return { args }
+    },
+    template: `
+      <Button v-bind="args">
+        <template v-slot:icon-right><ChevronDown/></template>
+      </Button>
+    `
+  })
+}
+
+export const BothIcon: Story = {
+  args: {
+    primary: true,
+    label: 'Button'
+  },
+  render: (args) => ({
+    components: { Button, ChevronDown },
+    setup() {
+      return { args }
+    },
+    template: `
+      <Button v-bind="args">
+        <template v-slot:icon-left><ChevronDown/></template>
+        <template v-slot:icon-right><ChevronDown/></template>
+      </Button>
+    `
+  })
+}
